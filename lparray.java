8e2f8a233e27be917dd4b108af1c94233bfaa4bc
jegadeeppandiarajan <jegadeeppandiarajan2003@gmail.com>
2025-01-02 19:28:23 +0530

    first commit

---
+++ lparray.java
@@ -0,0 +1,11 @@
+public class lparray {
+    public static void main(String args[])
+    {
+       // Scanner sc = new Scanner(System.in);
+        String cars[]={"volvo","audi","bmw"};
+        for(int i=0; i<cars.length; i++)
+        {
+            System.out.println(cars[i]);
+        }
+    }
+}
