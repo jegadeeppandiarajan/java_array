567c5c05b7d3d53885c5b0c65e0f135e00337545
jegadeeppandiarajan <jegadeeppandiarajan2003@gmail.com>
2025-01-02 19:30:34 +0530

    added new car

--- lparray.java
+++ lparray.java
@@ -2,7 +2,7 @@ public class lparray {
     public static void main(String args[])
     {
        // Scanner sc = new Scanner(System.in);
-        String cars[]={"volvo","audi","bmw"};
+        String cars[]={"benz","audi","bmw"};
         for(int i=0; i<cars.length; i++)
         {
             System.out.println(cars[i]);
